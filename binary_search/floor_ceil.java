1a3690905be62c38d9899629de4399657b37a67b
JayaSanthosh7066 <jayasanthoshamalakanti@gmail.com>
2024-02-05 22:13:09 +0530

    fc

---
+++ binary_search/floor_ceil.java
@@ -0,0 +1,10 @@
+package binary_search;
+
+import java.util.*;
+
+public class floor_ceil {
+    public static void main(String[] args) {
+        int n;
+        Scanner sc = new Scanner(System.in);
+    }
+}
