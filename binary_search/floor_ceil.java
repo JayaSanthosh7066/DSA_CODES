75093af7bcb2aa1ac15c6edafe2d971028152a8c
JayaSanthosh7066 <jayasanthoshamalakanti@gmail.com>
2024-02-07 23:15:49 +0530

    sc

--- binary_search/floor_ceil.java
+++ binary_search/floor_ceil.java
@@ -4,7 +4,15 @@ import java.util.*;
 
 public class floor_ceil {
     public static void main(String[] args) {
-        int n;
+        int n, k;
         Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        int arr[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        k = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+        }
     }
 }
