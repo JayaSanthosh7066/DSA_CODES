96a7916a54594d5862ea39cdbb02aaca9ba3abf5
JayaSanthosh7066 <jayasanthoshamalakanti@gmail.com>
2024-02-08 12:15:09 +0530

    lower_upper

---
+++ lower.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class lower {
+
+    public static void main(String[] args) {
+
+        int n, mid = 0;
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        int arr[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int k = sc.nextInt();
+        int ans = 0;
+        int low = 0, high = n - 1;
+        while (low <= high) {
+            mid = (low + high) / 2;
+            if (arr[mid] >= k) {
+                ans = arr[mid];
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        System.out.println(ans);
+        sc.close();
+    }
+}
